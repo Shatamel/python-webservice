f4e13b25c2974764829d8bcf1b26e04e041a4b31
yburyndi <yburyndi@cisco.com>
2022-01-28 16:38:36 +0200

    PE-1 Added TypeError exception in places they occured

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ url_regex = "^[a-z][a-z0-9+\-.]*"
 def geturls(url):
     url = re.findall(url_regex, url)
     policy = Policy(url)
-    return json.dumps(policy.parse_policy())
+    try:
+        return json.dumps(policy.parse_policy())
+    except TypeError:
+        return "Non parsable URL has been provided. Please check your input."
 
 
 if __name__ == '__main__':
